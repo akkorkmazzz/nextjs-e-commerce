f4bc1226f0aa17c80e03c2675fa3a605b6bbd51f
Alper Korkmaz <alper.korkmaz@alo-tech.com>
2025-03-18 21:29:25 +0300

    update-18-03-25

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "NxtEcommerce";
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more.";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "NxtEcommerce is a modern and responsive ecommerce platform built with Next.js, Tailwind CSS, and TypeScript.";
+
+
+
+
---
+++ lib/data.ts
@@ -0,0 +1,23 @@
+const data = {
+    headerMenu: [
+        {
+            name: "Today's Deal",
+            href: '/search?tag=todays-deal',
+        },
+        {
+            name: 'New Arrivals',
+            href: '/search?tag=new-arrivals',
+        },
+        {
+            name: 'Featured Products',
+            href: '/search?tag=featured-products',
+        },
+        {
+            name: 'Best Sellers',
+            href: '/search?tag=best-sellers',
+        },
+        
+    ]
+}
+
+export default data;
