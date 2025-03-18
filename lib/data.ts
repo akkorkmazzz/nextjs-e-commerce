90032583acdff262f4e32bb809e75a00ace062d8
Alper Korkmaz <alper.korkmaz@alo-tech.com>
2025-03-18 22:03:41 +0300

    update7-18-03-25

--- lib/data.ts
+++ lib/data.ts
@@ -16,7 +16,29 @@ const data = {
             name: 'Best Sellers',
             href: '/search?tag=best-sellers',
         },
-        
+    ],
+    carousels: [
+        {
+            title: 'Most Popular Shoes For Sale',
+            buttonCaption: 'Shop Now',
+            image: '/images/banner3.jpg',
+            url: '/search?category=Shoes',
+            isPublished: true,
+        },
+        {
+            title: 'Best Seller in T-Shirts',
+            buttonCaption: 'Shop Now',
+            image: '/images/banner1.jpg',
+            url: '/search?category=T-Shirts',
+            isPublished: true,
+        },
+        {
+            title: 'Best Deals on Wrist Watches',
+            buttonCaption: 'See More',
+            image: '/images/banner2.jpg',
+            url: '/search?category=Wrist Watches',
+            isPublished: true,
+        },
     ]
 }
 
